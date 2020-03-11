0e1dd1e29fa3d279063904b18913a4495a819f31
wissam <wissam_ataleh@outlook.com>
2020-03-11 17:13:11 +0200

    extend colors with JOSA color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        'josa-blue': {
+            default: '#3897c0',
+            'dark': '#1a485b',
+            'veryDark' : '#163e4e',
+          },
+          'josa-warm-grey': {
+            'light': '#f7f6f5',
+            default: '#efedec',
+            'dark': '#aea79f',
+          },
+          'josa-black': '#2e3436',
+        }
+      }
+    },
   variants: {},
   plugins: []
 }
