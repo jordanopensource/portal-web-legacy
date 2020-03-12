767b6538a1e3a29697b60644b7ea24a42e93002d
wissam <wissam_ataleh@outlook.com>
2020-03-12 14:29:12 +0200

    Create Navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <nav>
+    <nuxt-link class="nav-link" tag="a" to="#">About JOSA</nuxt-link>
+    <nuxt-link class="nav-link" tag="a" to="#">Our work</nuxt-link>
+    <nuxt-link class="nav-link" tag="a" to="#">Take Action!</nuxt-link>
+    <nuxt-link class="nav-link" tag="a" to="#">Connect</nuxt-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style lang="css" scoped>
+
+.nav-link {
+  @apply text-white p-2 uppercase block;
+}
+
+@screen sm {
+  .nav-link {
+    @apply inline-block;
+  }
+}
+
+</style>
