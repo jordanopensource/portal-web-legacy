7de5ca670cf32772d9bbedcc9c4330ebc93002ca
wissam <wissam_ataleh@outlook.com>
2020-03-15 14:38:54 +0200

    Create ImpactBox component

---
+++ components/ImpactBox.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="w-full md:w-1/2 p-12 bg-white">
+
+    <div class="display-lead pb-8">The Jordan Open Source Association (JOSA) is a non-profit organization that works for the promotion and implementation of the Open Source principles in Jordan.</div>
+
+    <p>JOSA organizes activities and lead initiatives in different fields, including Open Source software and hardware, Open Content and Free Culture, and digital rights.</p>
+
+    <nuxt-link to="/" tag="a" class="button-primary">Know more about our impact</nuxt-link>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'ImpactBox',
+
+}
+</script>
+
+<style scoped>
+    p {
+        @apply pb-16
+    }
+</style>
