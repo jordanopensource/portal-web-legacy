8e63d512c3e65369cd10b272129570dfc4e4e6ae
wissam <wissam_ataleh@outlook.com>
2020-03-15 16:21:30 +0200

    Import InstagramFeed component

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="container">
-    <impactBox />
+  <div class="container my-8">
+    <div class="row ">
+      <impactBox class="w-full md:w-1/2 p-12"/>
+      <instagramFeed class="w-full md:w-1/2"/>
+    </div>
   </div>
 </template>
 
 <script>
 import ImpactBox from '~/components/ImpactBox';
+import InstagramFeed from '~/components/InstagramFeed';
+
 export default {
   components: {
-    impactBox: ImpactBox,
+    'impactBox': ImpactBox,
+    'instagramFeed': InstagramFeed,
   }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
