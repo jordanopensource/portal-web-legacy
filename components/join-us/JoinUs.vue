4894c2a1f2d5aec24438357d43c70da44d2c4ae3
wissam <wissam_ataleh@outlook.com>
2020-03-17 00:45:56 +0200

    Change breakpoint from md to lg

--- components/join-us/JoinUs.vue
+++ components/join-us/JoinUs.vue
@@ -3,13 +3,13 @@
     <div class="pt-12 pl-12 pr-4 w-full">
         <h2 class="title">Join Us</h2>
     </div>
-    <div class="px-12 w-full md:w-1/2">
+    <div class="px-12 w-full lg:w-1/2">
     <p class="mb-4"><strong>JOSA is made by the efforts of members and volunteers like you!</strong></p>
     <p class="mb-4">Do you love Open Source? Lead the change in Jordan by joining us:</p>
     <nuxt-link class="mb-4 button button-blue-full" to="#" tag="a">Join JOSA</nuxt-link>
     </div>
-    <div class="w-full px-12 md:w-1/2 md:pr-0 md:pl-15px relative">
-        <img id="bot-1" class="md:absolute" src="~/static/images/bots/bot-1.svg" alt="bot-1">
+    <div class="w-full px-12 lg:w-1/2 lg:pr-0 lg:pl-15px relative">
+        <img id="bot-1" class="lg:absolute" src="~/static/images/bots/bot-1.svg" alt="bot-1">
     </div>
 </div>
 </template>
@@ -39,7 +39,7 @@ export default {
         right: -20px;
         bottom: 0;
     }
-    @screen md {
+    @screen lg {
         #bot-1 {
             max-width: 500px;
         }
