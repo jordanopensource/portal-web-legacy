0a4d9ffb7f333fdaff969074a2061d9d96c8b608
wissam <wissam_ataleh@outlook.com>
2020-03-16 18:27:05 +0200

    Rename Programs component, Change path of Programs component

---
+++ components/programs/Program.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="flex items-start">
+    <img class="block max-w-full mr-4 w-20 h-auto flex-shrink-0" :src="thumbnail" alt="">
+    <div class="mb-6 flex-grow">
+        <h3>{{ title }}</h3>
+        <p class="font-bold block">{{ subtitle}}</p>
+        <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+ h3 {
+     @apply mb-6 text-josa-blue uppercase;
+ }
+</style>
