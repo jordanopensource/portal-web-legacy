eb3a097e931abb02b8aaad6357fba15da615875f
wissam <wissam_ataleh@outlook.com>
2020-03-17 20:54:04 +0200

    Implement email validation function

--- components/newsletter/NewsletterForm.vue
+++ components/newsletter/NewsletterForm.vue
@@ -25,8 +25,14 @@ export default {
     return {
       email: '',
       language: 'English',
+      reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/
     }
   },
+  methods: {
+    isEmailValid() {
+      return this.reg.test(this.email);
+    },
+  }
 }
 </script>
 
