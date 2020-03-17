0814fa8e9b29368dff831dd1643bc4a4a544dd1b
wissam <wissam_ataleh@outlook.com>
2020-03-17 20:12:10 +0200

    Create custom RadioButton component

---
+++ components/form-components/RadioButton.vue
@@ -0,0 +1,92 @@
+<template>
+<span @click="update">
+  <input type="radio" :id="id" :value="value">
+  <div class="check" :class="{'checked' : this.model == this.value }"></div>
+  <label :for="id">{{ label }}</label>
+</span>
+</template>
+
+<script>
+export default {
+  name: 'RadioButton',
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    model: {
+      default: ''
+    }
+  },
+  methods: {
+    update() {
+      this.$emit('onUpdate', this.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+input[type="radio"] {
+  @apply absolute hidden;
+
+}
+
+span {
+  @apply text-josa-black block relative h-auto float-left mr-4;
+}
+
+input[type=radio] {
+  position: absolute;
+  visibility: hidden;
+}
+
+label {
+  @apply block relative cursor-pointer;
+  padding: 4px 0 4px 40px;
+
+  z-index: 9;
+  -webkit-transition: all 0.25s linear;
+}
+
+span:hover label {
+  @apply text-josa-blue
+}
+
+.check {
+  @apply border border-josa-blue border-solid rounded;
+  display: block;
+  position: absolute;
+  height: 30px;
+  width: 30px;
+  z-index: 5;
+}
+
+.check::before {
+  content: '';
+  @apply rounded block absolute;
+  height: 20px;
+  width: 20px;
+  top: 4px;
+  left: 4px;
+  margin: auto;
+  transition: background 0.25s linear;
+  -webkit-transition: background 0.25s linear;
+}
+
+span:hover .check::before {
+  @apply bg-josa-blue;
+}
+
+.checked::before {
+  @apply bg-josa-blue;
+}
+</style>
