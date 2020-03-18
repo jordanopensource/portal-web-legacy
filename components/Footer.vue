ddef10f527c1cb7e43d9d9d6839f9c8de64ea1e4
wissam <wissam_ataleh@outlook.com>
2020-03-18 03:01:30 +0200

    Create Footer component

---
+++ components/Footer.vue
@@ -0,0 +1,164 @@
+<template>
+<div id="footer">
+  <div class="container flex flex-col md:flex-row flex-wrap md:flex-no-wrap justify-between ">
+
+    <section>
+      <h6>About JOSA</h6>
+      <ul class="links">
+        <nuxt-link v-for="(link, label) in aboutLinks" :key="label" tag="li" :to='link' active-class="active"><a>{{ label }}</a></nuxt-link>
+      </ul>
+    </section>
+
+    <div class="block md:hidden py-8">
+      <hr>
+    </div>
+
+    <section>
+      <h6>Our Work</h6>
+      <ul class="links pb-4 md:pb-0">
+        <nuxt-link v-for="(link, label) in workLinks1" :key="label" tag="li" :to='link' active-class="active"><a>{{ label }}</a></nuxt-link>
+      </ul>
+      <div class="hidden md:block py-4">
+        <hr>
+      </div>
+      <ul class="links">
+        <nuxt-link v-for="(link, label) in workLinks2" :key="label" tag="li" :to='link' active-class="active"><a>{{ label }}</a></nuxt-link>
+      </ul>
+    </section>
+
+    <div class="block md:hidden py-8">
+      <hr>
+    </div>
+
+    <section>
+      <h6>Take Action</h6>
+      <ul class="links">
+        <nuxt-link v-for="(link, label) in actionLinks" :key="label" tag="li" :to='link' active-class="active"><a>{{ label }}</a></nuxt-link>
+      </ul>
+    </section>
+
+    <div class="block md:hidden py-8">
+      <hr>
+    </div>
+
+    <section>
+      <h6>Connect</h6>
+      <ul class="links">
+        <nuxt-link v-for="(link, label) in connectLinks" :key="label" tag="li" :to='link' active-class="active"><a>{{ label }}</a></nuxt-link>
+      </ul>
+      <div class="hidden md:block py-4">
+        <hr>
+      </div>
+    </section>
+
+  </div>
+
+  <div id="site-info" class="bg-josa-blue-veryDark">
+    <div class="container flex flex-col md:flex-row">
+      <div  class="logo">
+        <img src="~/static/logo/logo.svg" alt="JOSA Logo">
+      </div>
+      <div class="block md:hidden py-8">
+        <hr>
+      </div>
+      <div id="copyright-info" class="">
+        <img class="cc-logo pr-4" src="~/static/logo/cc.svg" alt="">
+        <p class="text-sm text-josa-warm-grey-dark">All the content on this website is licensed under a Creative Commons Attribution-ShareAlike Unported license, except where otherwise mentioned.</p>
+      </div>
+    </div>
+  </div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      aboutLinks: {
+        'News': '/',
+        'Blog': '/',
+        'Partners and Supporters': '/',
+        'Board and Staff': '/',
+        'Annual Reports': '/',
+        'Code of Conduct': '/',
+      },
+      workLinks1: {
+        'Open Source Software': '/',
+        'Open Source Hardware': '/',
+        'Open Data': '/',
+        'Open Internet': '/',
+        'Open Content': '/',
+      },
+      workLinks2: {
+        'Projects and Initiaves': '/',
+        'Events': '/',
+        'Publications': '/',
+      },
+      actionLinks: {
+        'Become a Member': '/',
+        "Member's Hub": '/',
+        'Volunteer': '/',
+        'Suggest a Speaker': '/',
+        'Donate': '/',
+      },
+      connectLinks: {
+        'Contact Us': '/',
+        'Join Slack': '/',
+        'Newsletter': '/',
+      },
+    }
+  }
+}
+</script>
+
+<style scoped>
+#footer {
+  @apply bg-josa-blue-dark text-white font-lato;
+  line-height: 1.6;
+}
+
+.container {
+  @apply p-16 !important;
+}
+#site-info .container {
+  @apply py-10 !important;
+}
+section {
+  @apply flex-grow;
+}
+
+h6 {
+  @apply uppercase pb-2;
+}
+
+hr {
+  @apply border-dotted
+}
+
+img {
+  max-width: 240px;
+}
+.logo, #copyright-info {
+    @apply w-full flex align-middle justify-center;
+}
+@screen md {
+  .logo, #copyright-info {
+   @apply w-1/2 justify-start;
+  }
+}
+@screen md {
+  section {
+    @apply px-4;
+  }
+}
+@screen lg {
+  .logo {
+   @apply w-2/3;
+  }
+  #copyright-info {
+    @apply w-1/3;
+  }
+}
+</style>
