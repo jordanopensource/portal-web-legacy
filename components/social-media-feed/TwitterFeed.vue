73ea460f2f6efd93bcb3136174bfe04fe425fd4f
wissam <wissam_ataleh@outlook.com>
2020-03-19 12:16:32 +0200

    Create TwitterFeed component

---
+++ components/social-media-feed/TwitterFeed.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="twitter-feed">
+        Recent Tweets by JOSA
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TwitterFeed',
+}
+</script>
+
+<style>
+
+</style>
