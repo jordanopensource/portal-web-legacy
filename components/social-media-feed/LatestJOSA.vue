c8685cb03d955cde1793f347907656ef574e29c9
wissam <wissam_ataleh@outlook.com>
2020-04-09 03:24:42 +0300

    Add language and count filters to loadedPostes

--- components/social-media-feed/LatestJOSA.vue
+++ components/social-media-feed/LatestJOSA.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-josa-warm-grey">
         <h2 class="title">{{ $t('socialFeed.title') }}</h2>
-        <socialFeed v-for="post in loadedPosts" 
+        <socialFeed v-for="post in loadedPosts"
         :key="post.id" 
         :id="post.id" 
         :src="post.src" 
@@ -20,8 +20,14 @@ export default {
     },
     computed: {
         loadedPosts() {
-            return this.$store.getters.loadedPosts
-        }
+            var locale = this.$i18n.locale
+            var posts = this.$store.getters.loadedPosts
+            var localizedPosts = posts.filter(element => element.lang == locale)
+            var fbPosts = localizedPosts.filter(element => element.src == 'fb').slice(0,2)
+            var twPosts = localizedPosts.filter(element => element.src == 'tw').slice(0,2)
+            var finalPosts = fbPosts.concat(twPosts)
+            return finalPosts
+        },
     }
 }
 </script>
