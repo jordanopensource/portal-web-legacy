7abd822a9d56b300ccb67e949fae0ea557f87be9
wissam <wissam_ataleh@outlook.com>
2020-04-29 19:59:08 +0300

    Create appImage component

---
+++ components/UI/appImage.vue
@@ -0,0 +1,33 @@
+<template>
+    <img :src="imageUrl" 
+    :alt="image.alternativeText">
+</template>
+
+<script>
+export default {
+    props: {
+        image: {
+            type: Object,
+            required: true
+        },
+        size: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        format() {
+            const size = this.size;
+            const format = this.image.formats[size];
+            return format
+        },
+        imageUrl() {
+            return process.env.baseUrl + this.format.url
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
