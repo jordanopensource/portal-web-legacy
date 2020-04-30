7d2ec8f895c6f67385b472c2cf5c8d089b7812c1
wissam <wissam_ataleh@outlook.com>
2020-04-30 20:45:31 +0300

    Implement Blogs views

---
+++ pages/blogs/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="blogs-page container">
+    <div class="container flex justify-between flex-wrap content-center mb-4">
+    <h2 class="title m-0">Blogs</h2>
+    </div>
+      <div>
+        <BlogList :blogs="loadedBlogs" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BlogList from "@/components/Blogs/BlogList";
+export default {
+  layout: "general",
+  components: {
+    BlogList,
+  },
+  computed: {
+    loadedBlogs() {
+      return this.$store.getters.loadedBlogs
+    }
+  },
+  created() {
+    this.$store.dispatch('fetchBlogs')
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
