4f796946d080e996fbf74f25e02343b1b456555e
wissam <wissam_ataleh@outlook.com>
2020-04-30 20:45:51 +0300

    Implement Blogs store

--- store/index.js
+++ store/index.js
@@ -1,10 +1,12 @@
 import Vuex from 'vuex';
 import socialFeed from './modules/social-feed';
+import blogs from './modules/blogs';
 
 const createStore = () => {
   return new Vuex.Store({
     modules: {
-        socialFeed
+        socialFeed,
+        blogs
     }
   })
 }
