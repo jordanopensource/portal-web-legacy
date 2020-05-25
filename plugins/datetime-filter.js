cfc2e7a99de4a7e673ac7daa4b736c447e1df28e
wissam <wissam_ataleh@outlook.com>
2020-05-25 23:38:20 +0300

    Create monthYear date filter

--- plugins/datetime-filter.js
+++ plugins/datetime-filter.js
@@ -5,4 +5,9 @@ const formatDateTime = dt => {
     return moment(dt).format("dddd, MMMM Do YYYY, h:mm a");
 };
 
+const monthYear = dt => {
+    return moment(dt).format("MMM YYYY");
+}
+
+Vue.filter('monthYear', monthYear)
 Vue.filter('dateTime', formatDateTime)
