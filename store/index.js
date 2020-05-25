ae26ac9f02b491aacbc3ac0f6cd2f30c0d25fb4d
wissam <wissam_ataleh@outlook.com>
2020-05-25 23:38:39 +0300

    Create members store

--- store/index.js
+++ store/index.js
@@ -1,11 +1,13 @@
 import Vuex from 'vuex';
 import socialFeed from './modules/social-feed';
 import events from './modules/events';
+import members from './modules/members';
 const createStore = () => {
   return new Vuex.Store({
     modules: {
         socialFeed,
-        events
+        events,
+        members
     }
   })
 }
