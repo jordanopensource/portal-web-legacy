30a827dbbad18e56c1345872e848c8bdea2d3c08
wissam <wissam_ataleh@outlook.com>
2020-05-27 00:33:48 +0300

    Add events create edit delete functionality

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -2,6 +2,7 @@
   <div class="events-page container">
     <div class="container flex justify-between flex-wrap content-center mb-4">
     <h2 class="title m-0">Events</h2>
+    <AppButton btn-style="button-blue-full" @click="$router.push('/events/create')">Create Event</AppButton>
     </div>
       <div>
         <EventList :events="loadedEvents" />
@@ -11,10 +12,12 @@
 
 <script>
 import EventList from "@/components/Events/EventList";
+import AppButton from '@/components/form-components/AppButton';
 export default {
   layout: "general",
   components: {
     EventList,
+    AppButton
   },
   computed: {
     loadedEvents() {
