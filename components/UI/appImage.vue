088dd01b62b7c75d1b9b4cb89c81ca7c7af07f0c
wissam <wissam_ataleh@outlook.com>
2020-06-09 10:21:13 +0300

    Add size fallback to image component

--- components/UI/appImage.vue
+++ components/UI/appImage.vue
@@ -1,31 +1,38 @@
 <template>
-    <img :src="imageUrl" 
-    :alt="image.alternativeText">
+  <img :src="imageUrl" :alt="image.alternativeText">
 </template>
 
 <script>
-export default {
+  export default {
     props: {
-        image: {
-            type: Object,
-            required: true
-        },
-        size: {
-            type: String,
-            required: true
-        }
+      image: {
+        type: Object,
+        required: true
+      },
+      size: {
+        type: String,
+        required: true
+      }
     },
     computed: {
-        format() {
-            const size = this.size;
-            const format = this.image.formats[size];
-            return format
-        },
-        imageUrl() {
-            return process.env.baseUrl + this.format.url
+
+      format() {
+        const size = this.size;
+        const format = this.image.formats[size];
+        if (format) {
+          return format
+        } else {
+          const formats = this.image.formats;
+          const first = Object.keys(formats)[0]
+          return formats[first]
         }
+      },
+      imageUrl() {
+        return process.env.baseUrl + this.format.url
+      }
     }
-}
+  }
+
 </script>
 
 <style>
