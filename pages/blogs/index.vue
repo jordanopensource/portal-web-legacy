318b323663ac95caa7fcf63d6ab95c50bf43b9a1
wissam <wissam_ataleh@outlook.com>
2020-06-18 17:10:23 +0300

    Add page banner to blog page

--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -1,20 +1,47 @@
 <template>
   <div class="blog-page container">
-    <blogList class="blog-list" title="featured" category="all" featured/>
+    <pageBanner :pageMeta="blogMeta" />
+    <blogList class="blog-list" title="featured" category="all" featured />
     <blogList class="blog-list" :title="$t('blog.lists.latest')" :numberOfArticles="2" category="press_releases" />
     <blogList class="blog-list" :title="$t('blog.lists.community')" :numberOfArticles="2" category="community" />
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
   import blogList from "@/components/Blogs/BlogList";
   import appButton from '@/components/form-components/AppButton';
+  import pageBanner from "@/components/UI/PageBanner";
   export default {
+    data() {
+      return {
+        title: "Blog"
+      }
+    },
+    head() {
+      return {
+        title: this.title,
+        meta: [{
+          hid: 'blog',
+          name: 'blog'
+        }]
+      }
+    },
     layout: "general",
     components: {
       blogList,
-      appButton
-    }
+      appButton,
+      pageBanner
+    },
+    asyncData(context) {
+      return axios.get(process.env.baseUrl + '/page-metas?title=blog')
+        .then(res => {
+          return {
+            blogMeta: res.data[0]
+          }
+        })
+        .catch(e => context.error(e))
+    },
   };
 
 </script>
