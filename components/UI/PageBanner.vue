3a56daedfd2bc46b9dfd649b1eb557a4c8f2962f
wissam <wissam_ataleh@outlook.com>
2020-06-29 03:23:00 +0300

    Change structure and add background color to banner

--- components/UI/PageBanner.vue
+++ components/UI/PageBanner.vue
@@ -1,10 +1,15 @@
 <template>
-<div class="flex flex-row justify-between align-middle flex-wrap sm:flex-no-wrap pt-20 pb-40 px-12">
-  <div class="my-auto sm:max-w-1/2 lg:max-w-1/4">
-  <h4 class="font-aleoLightItalic mb-16">{{ pageMeta.title | capitalize }}</h4>
-  <p class="leading-none">{{ pageMeta.description }}</p>
-  </div>
-  <appImage :image="pageMeta.image" class="bot mx-auto sm:mx-0 mt-12 sm:mt-0"/>
+  <div id="banner" class="bg-josa-warm-grey-light">
+    <div class="container">
+      <div
+        class="flex flex-row justify-between align-middle flex-wrap sm:flex-no-wrap pt-20 pb-40 px-12 bg-josa-warm-grey-light">
+        <div class="my-auto sm:max-w-1/2 lg:max-w-1/4">
+          <h4 class="font-aleoLightItalic mb-16">{{ pageMeta.title | capitalize }}</h4>
+          <p class="leading-none">{{ pageMeta.description }}</p>
+        </div>
+        <appImage :image="pageMeta.image" class="bot mx-auto sm:mx-0 mt-12 sm:mt-0" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,12 +18,12 @@
   export default {
     props: {
       pageMeta: {
-          type: Object,
-          required: true
+        type: Object,
+        required: true
       }
     },
     components: {
-        appImage
+      appImage
     }
   }
 
@@ -29,9 +34,11 @@
     width: 350px;
     height: auto;
   }
+
   @screen lg {
     .bot {
       width: 400px;
     }
   }
+
 </style>
