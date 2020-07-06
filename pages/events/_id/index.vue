69a495cd862bd37d2eb1f27dbc9ae72649af5ae7
wissam <wissam_ataleh@outlook.com>
2020-07-06 13:42:01 +0300

    Remove edit and create and delete functions

--- pages/events/_id/index.vue
+++ pages/events/_id/index.vue
@@ -2,14 +2,6 @@
   <div class="container">
     <appImage v-if="event.thumbnail" :image="event.thumbnail" size="medium" />
       <h2 class="event-name inline-block pr-2">{{ event.name }}</h2>
-      <span>
-        <nuxt-link to="edit" append>
-          <font-awesome-icon class="mr-2" :icon="['fas', 'edit']"/>
-        </nuxt-link>
-        <span @click="deleteEvent">
-          <font-awesome-icon class="mr-2" :icon="['fas', 'trash-alt']"/>
-        </span>
-      </span>
       <div class="event-details">
         <div>Event Date: {{ event.date | dateTime }}</div>
         <div>Location: {{ event.location }}</div>
@@ -37,12 +29,6 @@ export default {
     appImage
   },
   methods: {
-    deleteEvent() {
-      const eventId = this.$route.params.id
-      this.$store.dispatch("deleteEvent", eventId).then(() => {
-        this.$router.push("/events");
-      });
-    }
   }
 };
 </script>
--- pages/events/index.vue
+++ pages/events/index.vue
@@ -2,7 +2,6 @@
   <div class="events-page container">
     <div class="container flex justify-between flex-wrap content-center mb-4">
     <h2 class="title m-0">Events</h2>
-    <AppButton btn-style="button-blue-full" @click="$router.push('/events/create')">Create Event</AppButton>
     </div>
       <div>
         <EventList :events="loadedEvents" />
