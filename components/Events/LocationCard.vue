a2c961663a018109d0211d5ef5edd4fc6bc54a9c
wissam <wissam_ataleh@outlook.com>
2020-07-08 00:01:42 +0300

    Add address and map to event single page

--- components/Events/LocationCard.vue
+++ components/Events/LocationCard.vue
@@ -1,22 +1,32 @@
 <template>
-  <div class="location-card">
-    <font-awesome-icon class="icon" :icon="['fas', 'map-marker-alt']" />
-    <div>
-      <p class="font-bold leading-none">{{ $t('locationCard.title') }}</p>
-      <p>{{ location }}</p>
+  <div>
+    <locationMap v-if="address.latitude && address.longitude" :latitude="address.latitude" :longitude="address.longitude" :mLongitude="address.markerLongitude"
+      :mLatitude="address.markerLatitude" />
+    <div class="location-card">
+      <font-awesome-icon class="icon" :icon="['fas', 'map-marker-alt']" />
+      <div>
+        <p class="font-bold leading-none">{{ $t('locationCard.title') }}</p>
+        <p v-if="address.addressOne">{{ address.addressOne }}</p>
+        <p v-if="address.addressTwo" class="pt-1">{{ address.addressTwo }}</p>
+        <p v-if="address.street" class="pt-2">{{ address.street }}<span v-if="address.city">, {{ address.city }}</span></p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import locationMap from '~/components/Map/LocationMap';
   export default {
     name: 'Location',
     props: {
-      location: {
-        type: String,
+      address: {
+        type: Object,
         required: true
       }
-    }
+    },
+    components: {
+      locationMap
+    },
   }
 
 </script>
@@ -26,7 +36,9 @@
     @apply bg-josa-warm-grey-light p-8 flex align-top;
 
   }
+
   .icon {
     @apply text-josa-warm-grey-dark text-2xl mr-3;
   }
+
 </style>
