2dc92597c13fa306c707ba36880cb69e1850e127
Wissam <wissam_ataleh@outlook.com>
2021-01-11 18:07:59 +0200

    Add test matomo tracker

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,8 @@ export default {
       { rel: 'stylesheet', href:"https://unpkg.com/leaflet@1.6.0/dist/leaflet.css", integrity:"sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==", crossorigin:"" }
     ],
     script: [
-      { src:'/js/owa.js' }
+      { src:'/js/owa.js' },
+      { src:'/js/matomo.js' }
     ],
   },
   /*
