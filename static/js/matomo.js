1f5627ea95ac838394457a1f5ade5dabb53cd989
Wissam <wissam_ataleh@outlook.com>
2021-02-22 17:05:42 +0200

    Add JOSA analytics tracker script

--- static/js/matomo.js
+++ static/js/matomo.js
@@ -1,9 +1,12 @@
+// JOSA Analytics
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDomains", ["*.jordanopensource.org","*.www.jordanopensource.org"]]);
+  _paq.push(["enableCrossDomainLinking"]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//matomo.t1.k8s.jordanopensource.org/";
+    var u="//analytics.jordanopensource.org/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '2']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
