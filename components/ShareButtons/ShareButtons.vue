78f3254a318d216042e8e31228fb04f0c35067ba
wissam <wissam_ataleh@outlook.com>
2021-04-13 21:35:25 +0300

    Enhance style

--- components/ShareButtons/ShareButtons.vue
+++ components/ShareButtons/ShareButtons.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="md:flex frame">
-        <a class="share-buttons" :href="'https://twitter.com/share?url=' +url" target="_blank">
+    <div class="flex flex-wrap">
+        <a class="share-button" :href="'https://twitter.com/share?url=' + url" target="_blank">
             <font-awesome-icon :icon="['fab','twitter']" size="2x" />
         </a>
-        <a class="share-buttons" :href="'https://www.linkedin.com/shareArticle?url=' +url" target="_blank">
+        <a class="share-button" :href="'https://www.linkedin.com/shareArticle?url=' + url" target="_blank">
             <font-awesome-icon :icon="['fab','linkedin']" size="2x" />
         </a>
-        <a class="share-buttons" :href="'https://www.facebook.com/sharer.php?u=' +url" target="_blank">
+        <a class="share-button" :href="'https://www.facebook.com/sharer.php?u=' + url" target="_blank">
             <font-awesome-icon :icon="['fab','facebook-square']" size="2x" />
         </a>
         <span class="share-button tooltip" @click="copyToClipboard(url)">
@@ -43,27 +43,19 @@
 </script>
 
 <style scoped>
-.share-buttons{
-        @apply text-josa-warm-grey-dark;
-        @apply ml-4;
-        
-        cursor: pointer;
+    .share-button {
+        @apply text-josa-warm-grey-dark cursor-pointer;
     }
 
-.share-buttons:hover{
+    .share-button:hover {
         @apply text-josa-blue;
-        cursor: pointer;
     }
-    
-[dir="rtl"] .frame{
-    @apply justify-end ;
-    @apply flex-row-reverse;
-}  
-    article[dir="ltr"] {
-        @apply ml-4;
+
+    [dir="ltr"] .share-button {
+        @apply mr-4 ml-0;
     }
-    article[dir="rtl"] {
-        @apply mr-4;
+
+    [dir="rtl"] .share-button {
+        @apply ml-4 mr-0;
     }
-    
 </style>
