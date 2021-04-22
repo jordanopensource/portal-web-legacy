a1b85c3642d8781b566f0394445f38c32b7909f8
fmubaidien <mubaidien@gmail.com>
2021-04-22 03:34:03 +0300

    Add translators to article

--- components/Blog/BlogSingle.vue
+++ components/Blog/BlogSingle.vue
@@ -31,6 +31,13 @@
           :picture="author.picture" :bio="author['bio_' + $i18n.locale]" hide="true"/>
       </div>
     </div>
+    <div v-if="article.translators.length" class="py-0 authors"> 
+      <h5 class="pb-2 rtl:pr-4 ltr:pl-4 written-by text-base"> {{$t('meta.translatedBy')}}</h5>
+      <div class="flex flex-col">
+        <author v-for="translator in article.translators" v-bind:key="translator.id" class="mb-4" :name="translator['name_' + $i18n.locale]"
+          :picture="translator.picture" :bio="translator['bio_' + $i18n.locale]" />
+      </div>
+    </div>
   </div>
 </template>
 
