21dc3b3882c0a72b74bd115a866f1831ae5cc80f
fmubaidien <mubaidien@gmail.com>
2021-04-22 17:03:05 +0300

    Made else if instead of if in author

--- components/Blog/Author.vue
+++ components/Blog/Author.vue
@@ -3,7 +3,7 @@
     <appImage v-if="picture" :image="picture" size="small" class="profilePicture" />
     <div class="opacity-90">
       <p v-if="writtenBy" class="block ltr:text-xs rtl:text-sm uppercase">{{ $t('meta.writtenBy')}}</p>
-      <p v-if="translatedBy" class="block ltr:text-xs rtl:text-sm uppercase">{{ $t('meta.translatedBy')}}</p>
+      <p v-else-if="translatedBy" class="block ltr:text-xs rtl:text-sm uppercase">{{ $t('meta.translatedBy')}}</p>
       <h3 class="font-bold">{{ name }}</h3>
       <p v-if="bio">{{ bio }}</p>
     </div>
