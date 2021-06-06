bcd79b5fc9c0fc2c5d8d6f99d8515b768d1311eb
wissam <wissam_ataleh@outlook.com>
2021-06-06 21:58:49 +0300

    Refactor 'PublicationList' and 'Publications' page

--- components/Publications/PublicationList.vue
+++ components/Publications/PublicationList.vue
@@ -1,153 +1,49 @@
 <!-- Fetch and list publications based on publication categories-->
 <template>
-  <section v-if="ifNotEmpty()">
-    <div v-if="featured">
-      <h5 class="py-2 lg:pt-0 text-josa-blue">{{ title }}</h5>
-      <publicationFeatured v-for="publication in loadedPublications" :key="publication.id" :id="publication.id"
-        :publication="publication" />
-    </div>
-    <div v-else>
-      <h2>{{ title }}</h2>
-      <publicationPreview v-for="publication in loadedPublications" :key="publication.id" :id="publication.id"
-        :publication="publication" />
-      <!-- Pagination -->
-      <div v-if="pageCount > 1" class="pagination pt-6 text-center border-t border-dotted">
-        <span class="py-2" v-if="currentPage > 1"><a
-            @click="fetchCurrentPage(currentPage - 1)">{{ $t('pagination.prev') }}</a></span>
-        <span class="py-2 inline-flex ltr:text-left rtl:text-right"><a @click="fetchCurrentPage(i)"
-            v-for="i in pageCount" :key="i" class="p-2" :class="i == currentPage ? 'active' : ''">{{ $t(i) }}</a></span>
-        <span class="py-2" v-if="currentPage < pageCount"><a
-            @click="fetchCurrentPage(currentPage + 1)">{{ $t('pagination.next') }}</a></span>
-      </div>
-    </div>
+  <section>
+    <publicationPreview v-for="item in currentPageList" :key="item.id" :id="item.id" :publication="item" />
+    <Pagination :list="orderedList" @currentPage="setCurrentPage" />
   </section>
 </template>
 
 <script>
   import publicationPreview from '@/components/Publications/PublicationPreview';
-  import publicationFeatured from '@/components/Publications/PublicationFeatured';
-  import axios from 'axios';
+  import Pagination from '@/components/UI/Pagination';
+  import Vue2Filters from 'vue2-filters';
   export default {
+    mixins: [Vue2Filters.mixin],
     data() {
       return {
-        sortBy: 'publishDate:DESC',
-        loadedPublications: [],
-        currentPage: 1,
-        start: 0,
-        count: 0
+        currentPageList: [],
       }
     },
     components: {
       publicationPreview,
-      publicationFeatured
+      Pagination,
     },
     props: {
-      title: {
-        type: String,
+      publicationsList: {
+        type: Array,
         required: true
       },
-      limit: {
+      itemsPerPage: {
         type: Number,
-        default: 10
+        default: 5
       },
-      category: {
-        type: String,
-        default: 'all'
-      },
-      featured: {
-        type: Boolean,
-        default: false
+      sortBy: {
+        type: Array,
+        default: ['publishDate', -1],
       }
     },
     computed: {
-      pageCount() {
-        return Math.ceil(this.count / this.limit)
-      }
-    },
-    created() {
-      this.fetchPublications()
-      this.countPublications()
+      orderedList() {
+        return this.orderBy(this.publicationsList, this.sortBy[0], this.sortBy[1])
+      },
     },
     methods: {
-      fetchCurrentPage(i) {
-        this.currentPage = i
-        this.start = this.limit * (this.currentPage - 1)
-        this.fetchPublications()
+      setCurrentPage(list) {
+        this.currentPageList = list
       },
-      query() {
-        var args = []
-        var query = ""
-        if (this.start) {
-          let q = "_start=" + this.start;
-          args.push(q)
-        }
-        if (this.limit) {
-          let q = "_limit=" + this.limit;
-          args.push(q)
-        }
-        if (this.sortBy) {
-          let q = "_sort=" + this.sortBy;
-          args.push(q)
-        }
-        if (this.category && this.category != "all") {
-          let q = "category.name=" + this.category;
-          args.push(q)
-        }
-        if (this.featured) {
-          let q = "tags_contains=featured"
-          args.push(q)
-        }
-        query = args.join("&")
-        return query
-      },
-      async fetchPublications() {
-        const query = this.query()
-        await axios
-          .get(process.env.baseUrl + "/publications?" + query)
-          .then(res => {
-            const publicationsArray = []
-            for (const key in res.data) {
-              publicationsArray.push({
-                ...res.data[key]
-              })
-            }
-            this.loadedPublications = publicationsArray
-          })
-          .catch(e => this.context.error(e));
-      },
-      async countPublications() {
-        var args = []
-        var query = ""
-        if (this.category && this.category != "all") {
-          let q = "category.name=" + this.category;
-          args.push(q)
-        }
-        query = args.join("&")
-        await axios
-          .get(process.env.baseUrl + "/publications?" + query)
-          .then(res => {
-            this.count = res.data.length
-          })
-      },
-      ifNotEmpty() {
-        if (Array.isArray(this.loadedPublications) && this.loadedPublications.length)
-          return true;
-        else
-          return false;
-      },
-      limitNumberWithinRange(num, min, max) {
-        return Math.min(Math.max(parseInt(num), min), max)
-      },
-      calculateCurrentPage(num) {
-        this.currentPage = this.limitNumberWithinRange(num, 1, this.pageCount())
-        return this.currentPage
-      }
     }
   }
 </script>
-
-<style scoped>
-  .pagination a.active {
-    @apply text-josa-blue-veryDark;
-  }
-</style>
--- pages/publications/index.vue
+++ pages/publications/index.vue
@@ -2,37 +2,43 @@
   <div class="publication-page">
     <!-- Banner -->
     <pageBanner :pageMeta="pageInfo" />
-    <!-- Menu -->
+    <!-- Categories Menu -->
     <div class="bg-josa-black py-8">
       <div class="container">
         <div class="px-6 md:px-12 flex flex-col sm:flex-row">
           <span :class="activeCat == 'all' ? 'active': ''" class="cat-link"
             @click="setActiveCat('all')">{{ $t('publicationCats.title.all') }}</span>
-          <span :class="activeCat == cat.name ? 'active': ''" class="cat-link" v-for="cat in publicationCategories"
-            :key="cat.id" @click="setActiveCat(cat.name)">{{ cat['title_' + $i18n.locale] }}</span>
+          <span :class="activeCat == cat.name ? 'active': ''" class="cat-link" v-for="cat in categories" :key="cat.id"
+            @click="setActiveCat(cat.name)">{{ cat['title_' + $i18n.locale] }}</span>
         </div>
       </div>
     </div>
     <!-- List -->
     <div class="container pb-20">
-      <div v-for="cat in publicationCategories" :key="cat.id">
-        <publicationList v-if="activeCat=='all' || activeCat==cat.name" class="publication-list" :category="cat.name"
-          :title="cat['title_' + $i18n.locale]" />
-      </div>
+      <p v-if="$fetchState.pending" class="publication-list">{{ $t('meta.loading')+'...' }}</p>
+      <publicationList class="publication-list" v-if="$fetchState.timestamp && filteredList.length>=1"
+        :publicationsList="filteredList" :category="activeCat" :key="activeCat" />
+      <p v-if="!$fetchState.pending && filteredList.length<1" class="publication-list">{{ $t('meta.nothing')}}</p>
     </div>
   </div>
 </template>
 
 <script>
-  import axios from 'axios';
   import publicationList from "@/components/Publications/PublicationList";
-  import appButton from '@/components/FormComponents/AppButton';
   import pageBanner from "@/components/UI/PageBanner";
-
+  import Vue2Filters from 'vue2-filters';
   export default {
+    mixins: [Vue2Filters.mixin],
+    layout: "default",
+    components: {
+      publicationList,
+      pageBanner
+    },
     data() {
       return {
-        activeCat: 'all'
+        activeCat: 'all',
+        categories: null,
+        list: [],
       }
     },
     head() {
@@ -42,28 +48,36 @@
           'الجمعية الأردنية للمصدر المفتوح' : 'Jordan Open Source Association'),
         meta: [{
             name: 'description',
-            content: this.pageInfo['metaDescription_' + this.$i18n.locale] ? this.pageInfo['metaDescription_' + this.$i18n.locale] : ''
+            content: this.pageInfo['metaDescription_' + this.$i18n.locale] ? this.pageInfo['metaDescription_' + this
+              .$i18n.locale] : ''
           },
           ...i18nSeo.meta
         ]
       }
     },
-    layout: "default",
-    components: {
-      publicationList,
-      appButton,
-      pageBanner
-    },
     computed: {
       pageInfo() {
         return this.$store.state.pages.publications
-      }
+      },
+      filteredList() {
+        if (this.activeCat != "all" && this.list.length) {
+          return this.filterBy(this.list, this.activeCat, 'category.name')
+        } else {
+          return this.list
+        }
+      },
     },
-    async asyncData(context) {
-      const cats = await axios.get(process.env.baseUrl + '/publication-categories');
-      return {
-        publicationCategories: cats.data
+    async fetch() {
+      let cats = this.$store.state.publications.categories
+      if (!cats) {
+        await this.$store.dispatch("publications/fetchCats")
       }
+      this.categories = this.$store.state.publications.categories
+      let list = this.$store.state.publications.list
+      if (!list) {
+        await this.$store.dispatch("publications/fetchList")
+      }
+      this.list = this.$store.state.publications.list
     },
     methods: {
       setActiveCat(cat) {
