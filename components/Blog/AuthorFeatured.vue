bb2931ae665c60a2b58a21ff80e9e94ea6fd3f2d
Ashaqra <abdallashaqra2000@gmail.com>
2021-07-11 09:56:28 +0300

    Remove "BY" in english version and remove breakpoints in mobile version

--- components/Blog/AuthorFeatured.vue
+++ components/Blog/AuthorFeatured.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="md:flex flex-no-wrap items-center mt-4 ">  
+  <div class="flex flex-no-wrap items-center mt-4 ">  
     <appImage v-if="picture" :image="picture" size="small" class="profilePicture" />
     <img v-else size="small" class="profilePicture" src="/images/icons/authorimage_default.png" />
-    <div class="md:flex items-center opacity-80">
-      <p v-if="writtenBy" class="text-sm ltr:mr-1 uppercase">{{ $t('meta.by')}}</p>
-      <p v-else-if="translatedBy" class="ltr:mr-1 rtl:ml-1 text-xs uppercase">{{ $t('meta.translatedBy')}}</p>
+    <div class="flex items-center opacity-80">
+      <p v-if="translatedBy" class="ltr:mr-1 rtl:ml-1 text-xs uppercase">{{ $t('meta.translatedBy')}}</p>
       <p class="text-sm">{{ name }}</p>
       <p v-if="bio">{{ bio }}</p>
     </div>
