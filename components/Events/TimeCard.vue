d3b92451460f3432ef02afdcf8c6b549d7bc1c71
Issa Mahasneh <issa86@gmail.com>
2021-07-24 15:52:00 +0300

    Show non-Amman time only if timezone is different

--- components/Events/TimeCard.vue
+++ components/Events/TimeCard.vue
@@ -3,16 +3,19 @@
     <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'clock']" />
     <div>
       <h5>{{ $t('timeCard.title') }}</h5>
-      <p>{{ from | dayFullDate($i18n.locale) }}</p>
-      <p>{{ $t('timeCard.from') }} {{ from | time($i18n.locale) }} {{ $t('timeCard.to') }} {{ to | time($i18n.locale) }} 
-      ({{ $t('timeCard.gmt') }}{{ to | timezone($i18n.locale) }})</p>
-      <addToCalendar class="mt-4" :event="event"/>
+      <p>{{ from | dayFullDateAmman($i18n.locale) }}</p>
+      <p>{{ $t('timeCard.from') }} {{ from | timeAmman($i18n.locale) }} {{ $t('timeCard.to') }} {{ to | timeAmman($i18n.locale) }} ({{ $t('timeString.time') }})</p>
+      <br/>
+      <div v-if="new Date().getTimezoneOffset() !== -180">
+        <p>{{ from | dayFullDate($i18n.locale) }}</p>
+        <p>{{ $t('timeCard.from') }} {{ from | time($i18n.locale) }} {{ $t('timeCard.to') }} {{ to | time($i18n.locale) }}
+        ({{ $t('timeCard.gmt') }}{{ to | timezone($i18n.locale) }})</p>
+      </div>      
     </div>
   </div>
 </template>
 
 <script>
-  import addToCalendar from '~/components/Events/AddToCalendar';
   export default {
     name: 'TimeCard',
     props: {
@@ -21,15 +24,10 @@
       },
       to: {
         required: true
-      },
-      event:{
-        required: true
       }
-    },
-    components:{
-      addToCalendar
     }
   }
+
 </script>
 
 <style scoped>
@@ -37,7 +35,6 @@
     @apply bg-josa-warm-grey-light p-8 flex align-top;
 
   }
-
   .icon {
     @apply text-josa-warm-grey-dark text-2xl;
   }
