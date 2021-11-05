ccd0480d3bdb447a52854c546b08735e5d79da91
linuxjo <marina.alsahawneh@gmail.com>
2021-11-05 18:04:04 +0200

    Fix the vertical alignment of icons in the Careers Page

--- components/Careers/InfoCard.vue
+++ components/Careers/InfoCard.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="p-4 bg-josa-warm-grey-light">
     <div class="info-card">
-      <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'clock']" />
+      <div class="w-8">
+        <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'clock']" />
+      </div>
       <div>
         <h5 class="leading-none mb-1">{{ $t('careers.publishDate') }}</h5>
         <p>{{ career.publishDate | dayFullDate($i18n.locale) }}</p>
       </div>
     </div>
     <div class="info-card">
-      <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'map-marker-alt']" />
+      <div class="w-8 ltr:pl-1 rtl:pr-1">
+        <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'map-marker-alt']" />
+      </div>
       <div>
         <h5 class="leading-none mb-1">{{ $t('locationCard.title') }}</h5>
         <p>{{ career['location_' + $i18n.locale] }}</p>
       </div>
     </div>
     <div class="info-card">
-      <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'clipboard-list']" />
+      <div class="w-8 ltr:pl-1 rtl:pr-1">
+        <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'clipboard-list']" />
+      </div>
       <div>
         <h5 class="leading-none mb-1">{{ $t('careers.type') }}</h5>
         <p>{{ career.careerType['title_' + $i18n.locale] }}/ {{ career['period_' + $i18n.locale] }} </p>
       </div>
     </div>
     <div class="info-card">
-      <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'user']" />
+      <div class="w-8 ltr:pl-1 rtl:pr-1">
+        <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'user']" />
+      </div>
       <div>
         <h5 class="leading-none mb-1">{{ $t('careers.team') }}</h5>
         <p>{{ career.team['title_' + $i18n.locale] }}</p>
