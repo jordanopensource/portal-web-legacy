b77fb6fc0a790d8e2157e9dcccb26b5b7d22b4bd
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-23 12:27:46 +0200

    Refactor the preview components to use the latest tailwind classes

--- components/Partners/PartnerPreview.vue
+++ components/Partners/PartnerPreview.vue
@@ -1,60 +1,68 @@
 <template>
-  <div class="preview my-8 flex flex-wrap lg:flex-no-wrap">
-    <appImage v-if="partner.logo" :image="partner.logo" size="medium" class="thumbnail" />
+  <div class="preview my-8 flex flex-wrap lg:flex-nowrap">
+    <appImage
+      v-if="partner.logo"
+      :image="partner.logo"
+      size="medium"
+      class="thumbnail"
+    />
     <img v-else :src="placeholderImage" class="thumbnail" />
     <div class="flex-grow">
-      <h3 class="mb-4">{{ partner['title_' + $i18n.locale] }}</h3>
-      <p v-if="partner['description_' + $i18n.locale]">{{ partner['description_' + $i18n.locale] }}</p>
-      <p v-else>{{ partner['description_' + $i18n.locale] }}</p>
-      <a :href="partner.websiteUrl" target="_blank"
-        class="py-4 display-more">
-        {{ $t('meta.visitWebsite') }}
-        <font-awesome-icon class="ltr:ml-2 rtl:mr-2 align-middle" :icon="['fas', arrowIcon ]" />
+      <h3 class="mb-4">{{ partner["title_" + $i18n.locale] }}</h3>
+      <p v-if="partner['description_' + $i18n.locale]">
+        {{ partner["description_" + $i18n.locale] }}
+      </p>
+      <p v-else>{{ partner["description_" + $i18n.locale] }}</p>
+      <a :href="partner.websiteUrl" target="_blank" class="py-4 display-more">
+        {{ $t("meta.visitWebsite") }}
+        <font-awesome-icon
+          class="ltr:ml-2 rtl:mr-2 align-middle"
+          :icon="['fas', arrowIcon]"
+        />
       </a>
     </div>
   </div>
 </template>
 
 <script>
-  import appImage from '~/components/UI/appImage';
-  export default {
-    name: 'PartnerPreview',
-    data() {
-      return {
-        placeholderImage: process.env.bucketUrl + '/josabots_88f0a93786.jpeg'
+import appImage from "~/components/UI/appImage";
+export default {
+  name: "PartnerPreview",
+  data() {
+    return {
+      placeholderImage: process.env.bucketUrl + "/josabots_88f0a93786.jpeg",
+    };
+  },
+  components: {
+    appImage,
+  },
+  props: {
+    partner: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    arrowIcon() {
+      if (this.$i18n.locale == "ar") {
+        return "long-arrow-alt-left";
+      } else {
+        return "long-arrow-alt-right";
       }
     },
-    components: {
-      appImage
-    },
-    props: {
-      partner: {
-        type: Object,
-        required: true
-      }
-    },
-    computed: {
-      arrowIcon() {
-        if (this.$i18n.locale == "ar") {
-          return 'long-arrow-alt-left'
-        } else {
-          return 'long-arrow-alt-right'
-        }
-      }
-    }
-  }
-
+  },
+};
 </script>
 
 <style scoped>
-  .preview .thumbnail{
-    @apply object-contain bg-josa-warm-grey-light;
-  }
-  [dir="ltr"] .preview .thumbnail {
-    @apply pr-0 mr-6;
-  }
+.preview .thumbnail {
+  @apply object-contain bg-josa-warm-grey-light;
+}
+[dir="ltr"] .preview .thumbnail {
+  @apply pr-0 mr-6;
+}
 
-  [dir="rtl"] .preview .thumbnail {
-    @apply pl-0 ml-6;
-  }
+[dir="rtl"] .preview .thumbnail {
+  @apply pl-0 ml-6;
+}
 </style>
