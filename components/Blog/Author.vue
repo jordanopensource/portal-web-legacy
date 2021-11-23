841e765f0edd7110630afc0a74a545bbe4d4c0d2
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-23 12:32:59 +0200

    Refactor the author components to use the latest tailwind classes

--- components/Blog/Author.vue
+++ components/Blog/Author.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="flex flex-no-wrap items-center">
-    <appImage v-if="picture" :image="picture" size="small" class="profilePicture" />
-    <img v-else size="small" class="profilePicture" src="/images/icons/authorimage_default.png" />
+  <div class="flex flex-nowrap items-center">
+    <appImage
+      v-if="picture"
+      :image="picture"
+      size="small"
+      class="profilePicture"
+    />
+    <img
+      v-else
+      size="small"
+      class="profilePicture"
+      src="/images/icons/authorimage_default.png"
+    />
     <div class="opacity-90">
-      <p v-if="writtenBy" class="block ltr:text-xs rtl:text-sm uppercase">{{ $t('meta.writtenBy')}}</p>
-      <p v-else-if="translatedBy" class="block ltr:text-xs rtl:text-sm uppercase">{{ $t('meta.translatedBy')}}</p>
+      <p v-if="writtenBy" class="block ltr:text-xs rtl:text-sm uppercase">
+        {{ $t("meta.writtenBy") }}
+      </p>
+      <p
+        v-else-if="translatedBy"
+        class="block ltr:text-xs rtl:text-sm uppercase"
+      >
+        {{ $t("meta.translatedBy") }}
+      </p>
       <h4>{{ name }}</h4>
       <p v-if="bio">{{ bio }}</p>
     </div>
@@ -12,32 +29,31 @@
 </template>
 
 <script>
-  import appImage from '~/components/UI/appImage';
-  export default {
-    name: 'Author',
-    components: {
-      appImage
+import appImage from "~/components/UI/appImage";
+export default {
+  name: "Author",
+  components: {
+    appImage,
+  },
+  props: {
+    name: {
+      type: String,
     },
-    props: {
-      name: {
-        type: String,
-      },
-      picture: {
-        type: Object
-      },
-      bio: {
-        type: String
-      },
-      writtenBy: {
-        default: true
-      },
-      translatedBy: {
-        default: false
-      }
+    picture: {
+      type: Object,
     },
-    components: {
-      appImage
-    }
-  }
-
+    bio: {
+      type: String,
+    },
+    writtenBy: {
+      default: true,
+    },
+    translatedBy: {
+      default: false,
+    },
+  },
+  components: {
+    appImage,
+  },
+};
 </script>
--- components/Blog/AuthorFeatured.vue
+++ components/Blog/AuthorFeatured.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="flex flex-no-wrap items-center mb-2">  
-    <appImage v-if="picture" :image="picture" size="small" class="profilePicture" />
-    <img v-else size="small" class="profilePicture" src="/images/icons/authorimage_default.png" />
+  <div class="flex flex-nowrap items-center mb-2">
+    <appImage
+      v-if="picture"
+      :image="picture"
+      size="small"
+      class="profilePicture"
+    />
+    <img
+      v-else
+      size="small"
+      class="profilePicture"
+      src="/images/icons/authorimage_default.png"
+    />
     <div class="flex items-center opacity-80">
-      <p v-if="translatedBy" class="ltr:mr-1 rtl:ml-1 text-xs uppercase">{{ $t('meta.translatedBy')}}</p>
+      <p v-if="translatedBy" class="ltr:mr-1 rtl:ml-1 text-xs uppercase">
+        {{ $t("meta.translatedBy") }}
+      </p>
       <p class="text-sm">{{ name }}</p>
       <p v-if="bio">{{ bio }}</p>
     </div>
@@ -11,40 +23,39 @@
 </template>
 
 <script>
-  import appImage from '~/components/UI/appImage';
-  export default {
-    name: 'Author',
-    components: {
-      appImage
+import appImage from "~/components/UI/appImage";
+export default {
+  name: "Author",
+  components: {
+    appImage,
+  },
+  props: {
+    name: {
+      type: String,
     },
-    props: {
-      name: {
-        type: String,
-      },
-      picture: {
-        type: Object
-      },
-      bio: {
-        type: String
-      },
-      writtenBy: {
-        default: true
-      },
-      translatedBy: {
-        default: false
-      }
+    picture: {
+      type: Object,
     },
-    components: {
-      appImage
-    }
-  }
-
+    bio: {
+      type: String,
+    },
+    writtenBy: {
+      default: true,
+    },
+    translatedBy: {
+      default: false,
+    },
+  },
+  components: {
+    appImage,
+  },
+};
 </script>
 <style scoped>
-:dir(ltr).profilePicture{
-    @apply w-5 mr-1;
+:dir(ltr).profilePicture {
+  @apply w-5 mr-1;
 }
-:dir(rtl).profilePicture{
-    @apply w-5 ml-1;
+:dir(rtl).profilePicture {
+  @apply w-5 ml-1;
 }
 </style>
