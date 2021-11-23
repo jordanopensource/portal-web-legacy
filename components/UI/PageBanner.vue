6924c35fea9a198fdc31307a3261202cd02f0391
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-23 12:22:21 +0200

    Refactor the page banner component to use the latest tailwind classes

--- components/UI/PageBanner.vue
+++ components/UI/PageBanner.vue
@@ -2,43 +2,51 @@
   <div id="banner" class="bg-josa-warm-grey-light">
     <div class="container">
       <div
-        class="flex flex-row justify-between align-middle flex-wrap sm:flex-no-wrap py-12 md:py-6 px-6 md:px-12 bg-josa-warm-grey-light">
+        class="flex flex-row justify-between align-middle flex-wrap sm:flex-nowrap py-12 md:py-6 px-6 md:px-12 bg-josa-warm-grey-light"
+      >
         <div class="my-auto sm:max-w-1/2 lg:max-w-1/3">
-          <h1 class="mb-16">{{ pageMeta['title_' + $i18n.locale] }}</h1>
-          <p v-if="pageMeta['description_' + $i18n.locale]" class="ltr:leading-normal rtl:leading-relaxed">{{ pageMeta['description_' + $i18n.locale] }}</p>
+          <h1 class="mb-16">{{ pageMeta["title_" + $i18n.locale] }}</h1>
+          <p
+            v-if="pageMeta['description_' + $i18n.locale]"
+            class="ltr:leading-normal rtl:leading-relaxed"
+          >
+            {{ pageMeta["description_" + $i18n.locale] }}
+          </p>
         </div>
-        <appImage v-if="pageMeta.image" :image="pageMeta.image" class="bot mt-8 md:ltr:pl-8 md:rtl:pr-8 md:py-8" />
+        <appImage
+          v-if="pageMeta.image"
+          :image="pageMeta.image"
+          class="bot mt-8 md:ltr:pl-8 md:rtl:pr-8 md:py-8"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import appImage from '~/components/UI/appImage';
-  export default {
-    props: {
-      pageMeta: {
-        type: Object,
-        required: true
-      }
+import appImage from "~/components/UI/appImage";
+export default {
+  props: {
+    pageMeta: {
+      type: Object,
+      required: true,
     },
-    components: {
-      appImage
-    }
-  }
-
+  },
+  components: {
+    appImage,
+  },
+};
 </script>
 
 <style scoped>
+.bot {
+  width: 320px;
+  height: auto;
+}
+
+@screen lg {
   .bot {
-    width: 320px;
-    height: auto;
+    width: 350px;
   }
-
-  @screen lg {
-    .bot {
-      width: 350px;
-    }
-  }
-
+}
 </style>
--- components/UI/PageBannerNoImage.vue
+++ components/UI/PageBannerNoImage.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="banner" class="bg-josa-warm-grey-light">
     <div class="container">
-      <div class="flex flex-row justify-between align-middle flex-wrap md:flex-no-wrap px-6 md:px-12 py-20 lg:py-32 bg-josa-warm-grey-light">
-        <h1 class="w-full md:w-1/2 flex-shrink-0 mb-8">{{ pageMeta['title_' + $i18n.locale] }}</h1>
-        <p v-if="pageMeta['description_' + $i18n.locale]" class="md:ltr:pl-8 md:rtl:pr-8 w-full md:w-1/2 flex-shrink-0">
-          {{ pageMeta['description_' + $i18n.locale] }}</p>
+      <div
+        class="flex flex-row justify-between align-middle flex-wrap md:flex-nowrap px-6 md:px-12 py-20 lg:py-32 bg-josa-warm-grey-light"
+      >
+        <h1 class="w-full md:w-1/2 flex-shrink-0 mb-8">
+          {{ pageMeta["title_" + $i18n.locale] }}
+        </h1>
+        <p
+          v-if="pageMeta['description_' + $i18n.locale]"
+          class="md:ltr:pl-8 md:rtl:pr-8 w-full md:w-1/2 flex-shrink-0"
+        >
+          {{ pageMeta["description_" + $i18n.locale] }}
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      pageMeta: {
-        type: Object,
-        required: true
-      }
-    }
-  }
-
+export default {
+  props: {
+    pageMeta: {
+      type: Object,
+      required: true,
+    },
+  },
+};
 </script>
