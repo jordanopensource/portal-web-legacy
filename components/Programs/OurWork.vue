6a9fa1c1d7a7094c90e04b47aa01c141c47f7dca
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-23 12:26:25 +0200

    Refactor our work component to use the latest tailwind classes

--- components/Programs/OurWork.vue
+++ components/Programs/OurWork.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="our-work">
     <h2 class="pb-2" v-if="title">{{ title }}</h2>
-    <programsList class="grid grid-cols-1 md:grid-cols-2 col-gap-20" :programs="programs"/>
+    <programsList
+      class="grid grid-cols-1 md:grid-cols-2 gap-x-20"
+      :programs="programs"
+    />
   </div>
 </template>
 
 <script>
-  import programsList from '~/components/Programs/ProgramsList';
-  export default {
-    name: 'OurWork',
-    components: {
-      programsList,
+import programsList from "~/components/Programs/ProgramsList";
+export default {
+  name: "OurWork",
+  components: {
+    programsList,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
     },
-    props: {
-      title: {
-        type: String,
-        required: true
-      },
-      programs: {
-        type: Array,
-        required: true
-      }
-    }
-  }
-
+    programs: {
+      type: Array,
+      required: true,
+    },
+  },
+};
 </script>
