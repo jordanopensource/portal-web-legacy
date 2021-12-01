a498f90152364f5fc164e97a182cd6c8e13bad1e
linuxjo <marina.alsahawneh@gmail.com>
2021-12-01 15:42:32 +0200

    Change Icons and increase their width and margin

--- components/Careers/InfoCard.vue
+++ components/Careers/InfoCard.vue
@@ -2,36 +2,36 @@
   <div class="p-4 bg-josa-warm-grey-light">
     <div class="info-card">
       <div class="w-8">
-        <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'clock']" />
+        <font-awesome-icon class="fa-fw icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'calendar-day']" />
       </div>
-      <div>
+      <div class="ltr:ml-3 rtl:mr-3">
         <h5 class="leading-none mb-1">{{ $t('careers.publishDate') }}</h5>
         <p>{{ career.publishDate | dayFullDate($i18n.locale) }}</p>
       </div>
     </div>
     <div class="info-card">
-      <div class="w-8 ltr:pl-1 rtl:pr-1">
-        <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'map-marker-alt']" />
+      <div class="w-8">
+        <font-awesome-icon class="fa-fw icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'globe-europe']" />
       </div>
-      <div>
+      <div class="ltr:ml-3 rtl:mr-3">
         <h5 class="leading-none mb-1">{{ $t('locationCard.title') }}</h5>
         <p>{{ career['location_' + $i18n.locale] }}</p>
       </div>
     </div>
     <div class="info-card">
-      <div class="w-8 ltr:pl-1 rtl:pr-1">
-        <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'clipboard-list']" />
+      <div class="w-8">
+        <font-awesome-icon class="fa-fw icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'file-signature']" />
       </div>
-      <div>
+      <div class="ltr:ml-3 rtl:mr-3">
         <h5 class="leading-none mb-1">{{ $t('careers.type') }}</h5>
-        <p>{{ career.careerType['title_' + $i18n.locale] }}/ {{ career['period_' + $i18n.locale] }} </p>
+        <p>{{ career.careerType['title_' + $i18n.locale] }} {{ career['period_' + $i18n.locale] }} </p>
       </div>
     </div>
     <div class="info-card">
-      <div class="w-8 ltr:pl-1 rtl:pr-1">
-        <font-awesome-icon class="icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'user']" />
+      <div class="w-8">
+        <font-awesome-icon class="fa-fw icon ltr:mr-3 rtl:ml-3" :icon="['fas', 'user-friends']" />
       </div>
-      <div>
+      <div class="ltr:ml-3 rtl:mr-3">
         <h5 class="leading-none mb-1">{{ $t('careers.team') }}</h5>
         <p>{{ career.team['title_' + $i18n.locale] }}</p>
       </div>
